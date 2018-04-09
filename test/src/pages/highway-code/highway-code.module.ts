613add320e39c9f360e6e8184804413f3d77ccaf
“Richard Middleton” <Richard@richardmiddleton.me>
2018-04-09 10:29:44 +0200

    monday

---
+++ test/src/pages/highway-code/highway-code.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { HighwayCodePage } from './highway-code';
+
+@NgModule({
+  declarations: [
+    HighwayCodePage,
+  ],
+  imports: [
+    IonicPageModule.forChild(HighwayCodePage),
+  ],
+})
+export class HighwayCodePageModule {}
